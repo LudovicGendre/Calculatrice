1896fc77c131050c39a20b22f51d5bc40d1ce270
LudovicGendre <ludovic.gendre@g-mail.fr>
2020-06-05 15:47:33 +0200

    [Main.js] Creat Key and button calculatrice

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-var a = 1;
-console.log(a);
+{
+    function creatButton(text, className, container) {
+        var button = document.createElement('button');
+        button.textContent = text;
+        if (className) {
+            button.className = className;
+        }
+        container.appendChild(button);
+    }
+    var container_1 = document.createElement('div');
+    container_1.classList.add('cal');
+    document.body.appendChild(container_1);
+
+    var output = document.createElement('div');
+    var span = document.createElement('span');
+
+    output.classList.add('cal-output');
+    span.textContent = '1000';
+    output.appendChild(span);
+    container_1.appendChild(output);
+
+    var key = [
+        ['Clear', '/'],
+        ['7', '8', '9', 'x']
+    ]
+}
